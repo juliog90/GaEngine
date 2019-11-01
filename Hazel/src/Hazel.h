cb57677ff9b826ce2b54922911ebed2a5ddb79da
Julio Armando Gastelum Martinez <tjuliogastelum90@gmail.com>
2019-10-31 21:25:28 -0700

    First Commit

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use on Hazel applications
+
+#include "Hazel/Application.h"
+
+// -----Entry Point -----------
+#include "Hazel/EntryPoint.h"
+// ----------------------------
+
